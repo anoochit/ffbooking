6535089916ddde33a8f80ae5ae3c5c9d6b3b8974
Anuchit Chalothorn <anoochit@gmail.com>
2024-01-17 11:06:34 +0700

    complete tutorial

--- lib/app/data/models/event.dart
+++ lib/app/data/models/event.dart
@@ -1,22 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:ui';
 
 import 'package:syncfusion_flutter_calendar/calendar.dart';
-
-class Event {
-  String eventName;
-  DateTime from;
-  DateTime to;
-  Color background;
-  bool isAllDay;
-  Event({
-    required this.eventName,
-    required this.from,
-    required this.to,
-    required this.background,
-    required this.isAllDay,
-  });
-}
 
 class DataSource extends CalendarDataSource {
   DataSource(List<Appointment> source) {
--- lib/app/modules/home/views/calendar_view.dart
+++ lib/app/modules/home/views/calendar_view.dart
@@ -1,9 +1,11 @@
+import 'package:ffbooking/app/data/models/event.dart';
 import 'package:ffbooking/app/modules/home/controllers/home_controller.dart';
 import 'package:ffbooking/app/modules/home/views/navigation_drawer_view.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:responsive_builder/responsive_builder.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarPageView extends GetView<HomeController> {
   const CalendarPageView({super.key});
@@ -14,8 +16,26 @@ class CalendarPageView extends GetView<HomeController> {
         title: const Text('Calendar'),
       ),
       // TODO : add calendar view
-      body: Container(),
-
+      body: SfCalendar(
+        view: CalendarView.month,
+        allowedViews: [
+          CalendarView.day,
+          CalendarView.month,
+          CalendarView.schedule,
+          CalendarView.timelineDay,
+          CalendarView.timelineMonth,
+          CalendarView.timelineWeek,
+        ],
+        showNavigationArrow: true,
+        showDatePickerButton: true,
+        showTodayButton: true,
+        dataSource: _getDataSource(),
+        monthViewSettings: const MonthViewSettings(
+          appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
+          appointmentDisplayCount: 5,
+          // showAgenda: true,
+        ),
+      ),
       drawer: (controller.screenType.value == DeviceScreenType.mobile)
           ? const NavigationDrawerView()
           : null,
@@ -23,4 +43,44 @@ class CalendarPageView extends GetView<HomeController> {
   }
 
   // TODO : add mockup datasource
+  DataSource _getDataSource() {
+    List<Appointment> appointments = <Appointment>[];
+
+    appointments.add(
+      Appointment(
+        subject: 'Live record video',
+        startTime: DateTime.now(),
+        endTime: DateTime.now().add(
+          const Duration(hours: 2),
+        ),
+        color: Colors.blue,
+      ),
+    );
+
+    appointments.add(
+      Appointment(
+        subject: 'Meeting',
+        startTime: DateTime(2024, 1, 16),
+        endTime: DateTime(2024, 1, 18).add(
+          const Duration(hours: 2),
+        ),
+        isAllDay: true,
+        color: Colors.amber,
+      ),
+    );
+
+    appointments.add(
+      Appointment(
+        subject: 'Facebook Live',
+        startTime: DateTime(2024, 1, 16, 18, 0, 0),
+        endTime: DateTime(2024, 1, 16, 18, 0, 0).add(
+          const Duration(hours: 2),
+        ),
+        color: Colors.red,
+        recurrenceRule: 'FREQ=WEEKLY;INTERVAL=1;BYDAY=TU',
+      ),
+    );
+
+    return DataSource(appointments);
+  }
 }
